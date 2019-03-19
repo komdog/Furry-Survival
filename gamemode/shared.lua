9578b9a1d59179e377620e0154f45d2c20c690c1
komdog <42328288+komdog@users.noreply.github.com>
2019-03-18 20:36:11 -0400

    Updated Spawn System

--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -3,12 +3,26 @@ GM.Author = "KomDog"
 GM.Email = "N/A"
 GM.Website = "https://komdog.club"
 
-team.SetUp(0, "Waiting...", Color(100, 100, 100, 255))
-team.SetUp(1, "Furries", Color(150, 20, 20, 255))
-team.SetUp(2, "Normies", Color(100, 60, 240, 255))
-team.SetUp(3, "Animal Control", Color(240, 160, 20, 255))
-
-
 function GM:Initialize()
     self.BaseClass.Initialize(self)
+end
+
+function GM:CreateTeams()
+
+    TEAM_WAITING = 0
+    team.SetUp(0, "Waiting...", Color(100, 100, 100, 255))
+    team.SetSpawnPoint( TEAM_WAITING, { "info_n_spawn" } )
+
+    TEAM_FURRY = 1
+    team.SetUp(1, "Furries", Color(150, 20, 20, 255))
+    team.SetSpawnPoint( TEAM_FURRY, { "info_yiff_spawn" } )
+
+    TEAM_NORMIE = 2
+    team.SetUp(2, "Normies", Color(100, 60, 240, 255))
+    team.SetSpawnPoint( TEAM_NORMIE, { "info_n_spawn" } )
+
+    TEAM_AC = 3
+    team.SetUp(3, "Animal Control", Color(240, 160, 20, 255))
+    team.SetSpawnPoint( TEAM_AC, { "info_n_spawn" } )
+
 end
